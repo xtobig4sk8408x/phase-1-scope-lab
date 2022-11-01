d3e81160b3066d3b8a35ae6ec0ff59c3adf70ed8
Theodore John Josef <john.josef@aol.com>
2022-11-01 15:00:49 -0700

    commit tree 63cafa877bc468084f083498749fc42ad6a6821f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "";
+
+function upperCaseCustomerName () { 
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer () { 
+    leastFavoriteCustomer = 'bob';
+}
